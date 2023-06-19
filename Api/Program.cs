316f6fb23b2cce88e9c40aa884d8a90680bbcbfb
Hugo Biarge <hugo@arosbi.com>
2023-06-19 18:16:04 +0200

    Add Serilog

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,10 +1,19 @@
+using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Events;
+using Serilog.Exceptions;
+
 namespace Api
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            CreateLogger();
+
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseSerilog(dispose: true);
 
             // Add services to the container.
             builder.Services.AddRazorPages();
@@ -29,6 +38,20 @@ namespace Api
             app.MapRazorPages();
 
             app.Run();
+        }
+
+        private static void CreateLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .Enrich.WithSpan()
+                .Enrich.WithExceptionDetails()
+                .WriteTo.Console(new Serilog.Formatting.Json.JsonFormatter())
+                .CreateLogger();
         }
     }
 }
